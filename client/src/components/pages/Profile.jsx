50aa03a0457e9c25fbfcf41c70df513db8734199
Sobia51 <sobiasafdar51@gmail.com>
2022-12-13 00:50:01 +0500

    //

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import {Card} from 'antd'
-import Edit from '../Edit'
+
 function Profile() {
     
   return (
     <div className= 'Container' style={{backgroundColor:'#8BBDB428',padding:'1%',height:'100%',width:'100%'}}>
 <Card 
       title=<h3> "Team or Agency Name" </h3>
-      extra={<a href='Edit.jsx'>Edit</a>}
+      extra={<a href='Edit'>Edit</a>}
       style={{
         width: '40%',
         marginLeft:'25%',
