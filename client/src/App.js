5a65d982e4b1015e78103499a2e9afa0a9232d3b
Sobia51 <sobiasafdar51@gmail.com>
2022-12-13 00:48:55 +0500

    Updating Functions

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,9 @@ import Statics from './components/pages/Statics';
 import Subscription from './components/pages/Subscription';
 import Widget from './components/pages/Widget';
 import W_menu from './components/pages/W_menu';
+import Edit from './components/pages/Edit';
 import Sidebar from './components/Sidebar';
+import Feedback from './components/pages/Feedback';
 
 function App() {
   return (
@@ -22,6 +24,8 @@ function App() {
 <Route path ="/W_menu" element ={<W_menu/>}/>
 <Route path ="/Subscription" element ={<Subscription/>}/>
 <Route path ="/Profile" element ={<Profile/>}/>
+<Route path='/Edit' element={<Edit/>}/>
+<Route path='/Feedback' element={<Feedback/>}/>
 </Routes>
 </Sidebar>
       
